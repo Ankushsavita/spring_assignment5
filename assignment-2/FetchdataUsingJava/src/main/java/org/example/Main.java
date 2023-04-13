15ba685e87b2515633f2f431de3d7c75a3f21440
Ankush savita <ankushsavita2001@gmail.com>
2023-04-13 21:21:29 +0530

    done

---
+++ assignment-2/FetchdataUsingJava/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        String url = "https://api.chucknorris.io/jokes/random";
+
+        // Building a Request
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+
+        // Getting HttpClient Object
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+        HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // StatusCode and body
+        System.out.println(httpResponse.statusCode());
+        System.out.println(httpResponse.body());
+    }
+}
